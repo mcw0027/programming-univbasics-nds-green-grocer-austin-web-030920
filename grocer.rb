020269490800505ae5462a2095d160d27a1ec76e
Michael Wixson <wixsonmc@gmail.com>
2020-02-08 04:35:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -72,6 +72,7 @@ counter = 0
 while counter < cart.length
 if cart[counter][:clearance]
   cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.20)).round(2)
+end
 counter += 1
 end
 cart
