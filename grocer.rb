cf58411926c0a7b024865bd3927e12b0ed9dbbcc
Michael Wixson <wixsonmc@gmail.com>
2020-02-08 04:44:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -82,4 +82,8 @@ def checkout(cart, coupons)
 consolidated_cart = consolidate_cart(cart)
 couponed_cart = apply_coupons(consolidated_cart)
 final_cart = apply_clearance(couponed_cart)
+
+total = 0
+counter = 0
+while counter < final_cart
 end
